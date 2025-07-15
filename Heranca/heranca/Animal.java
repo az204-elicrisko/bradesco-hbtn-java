19931084340138c1d0dd85c35d8e5e9b8d9521f3
Eliane Cristina Kobayakawa <eliane.kobayakawa@bradesco.com.br>
2025-07-15 10:02:06 -0300

    task 0 heranca

---
+++ Heranca/heranca/Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Animal() {
+        this.nome = "Cachorro";
+    }
+
+    public Animal(String nome) {
+        this.nome = nome;
+    }
+
+}
