b3ced8f16af8aac83d3c8c5e13494f0726b4aaa7
Eliane Cristina Kobayakawa <az204-elicrisko@outlook.com>
2025-07-24 12:40:08 +0000

    ajuste

--- complexidade/0/PessoasArray.java
+++ complexidade/0/PessoasArray.java
@@ -21,7 +21,7 @@ public class PessoasArray {
     // implementar o mehtodo de pesquisaConstante
     public void pesquisaTempoConstante(int id) throws IllegalArgumentException {
         try {
-            System.out.println("Nome pesquisado é " + nomes[id] + " que está na posição " + id);
+            System.out.println("Nome pesquisado e " + nomes[id] + " que esta na posicao " + id);
         } catch (Exception ex) {
             throw new IllegalArgumentException("O array de nomes possui :" + nomes.length + " nomes.");
         }
