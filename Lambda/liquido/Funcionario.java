ba99092643200a08b73d8545cc56bd1be6bcabae
Eliane Cristina Kobayakawa <eliane.kobayakawa@bradesco.com.br>
2025-08-04 12:04:08 -0300

    task 1. Salario Funcionario

---
+++ Lambda/liquido/Funcionario.java
@@ -0,0 +1,12 @@
+public class Funcionario {
+
+    private double salario;
+
+    public Funcionario(double i) {
+        this.salario = i;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
