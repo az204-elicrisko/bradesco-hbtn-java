f01716e52a00af7a1a1d69ef4d6b61f4638e3ed0
Eliane Cristina Kobayakawa <eliane.kobayakawa@bradesco.com.br>
2025-08-04 14:29:06 -0300

    task 1. Consultar Produtos com Predicado

---
+++ Lambda_Exercicios/predicado/Produto.java
@@ -0,0 +1,39 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private double peso;
+    private int quantidadeEmEstoque;
+    private TiposProduto tipo;
+
+    public Produto(String nome, double preco, double peso, int quantidadeEmEstoque, TiposProduto tipo) {
+        this.nome = nome;
+        this.preco = preco;
+        this.peso = peso;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+        this.tipo = tipo;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public TiposProduto getTipo() {
+        return tipo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + String.format("%.6f", preco) + " " + String.format("%.6f", peso) +
+                " " + quantidadeEmEstoque + " " + tipo;
+    }
+}
