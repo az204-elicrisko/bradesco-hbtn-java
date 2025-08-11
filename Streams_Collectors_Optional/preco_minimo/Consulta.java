dca880e782b25c85159360642c1fe4ecd96027d0
Eliane Cristina Kobayakawa <eliane.kobayakawa@bradesco.com.br>
2025-08-11 17:18:57 -0300

    task 2. Relatório Produtos por preco minimo

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,25 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido1) {
+        return pedido1.getProdutos().stream()
+                .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
+                .toList();
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream()
+                .sorted(Comparator.comparingDouble(Produto::getPreco).reversed())
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Nenhum produto encontrado"));
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        return produtos.stream()
+                .filter(produto -> produto.getPreco() >= precoMinimo)
+                .toList();
+    }
+
+}
