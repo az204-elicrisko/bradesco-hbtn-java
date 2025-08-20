8a36ff1e3262b94000ee4959e7efe798cb959df1
Eliane Cristina Kobayakawa <eliane.kobayakawa@bradesco.com.br>
2025-08-20 15:56:27 -0300

    task 0. Pessoas agrupadas por cargo

---
+++ 0x19/pessoa_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+        Map<String, List<Pessoa>> pessoasPorCargo = pessoas.stream()
+            .collect(Collectors.groupingBy(Pessoa::getCargo));
+        return pessoasPorCargo;
+    }
+}
