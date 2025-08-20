c58abd7ca9cfc7135cdf71ce0fc2c77b2be3713f
Eliane Cristina Kobayakawa <eliane.kobayakawa@bradesco.com.br>
2025-08-20 16:00:04 -0300

    ajuste

--- 0x19/pessoa_por_cargo/ConsultaPessoas.java
+++ 0x19/pessoa_por_cargo/ConsultaPessoas.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 
 public class ConsultaPessoas {
     public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
-        Map<String, List<Pessoa>> pessoasPorCargo = pessoas.stream()
+    	Map<String, List<Pessoa>> pessoasPorCargo = pessoas.stream()
             .collect(Collectors.groupingBy(Pessoa::getCargo));
         return pessoasPorCargo;
     }
