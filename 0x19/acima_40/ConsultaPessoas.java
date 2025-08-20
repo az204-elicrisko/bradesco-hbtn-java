923d6a879568b085459aad039957eb93113d1a44
Eliane Cristina Kobayakawa <eliane.kobayakawa@bradesco.com.br>
2025-08-20 16:20:56 -0300

    task 2. Pessoas agrupadas por cargo e que possuam pelo menos 40 anos

---
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+    	Map<String, List<Pessoa>> pessoasPorCargo = pessoas.stream()
+            .collect(Collectors.groupingBy(Pessoa::getCargo));
+        return pessoasPorCargo;
+    }
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
+        Map<String, List<Integer>> idadesPorCargo = pessoas.stream()
+            .collect(Collectors.groupingBy(Pessoa::getCargo,
+                Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+        return idadesPorCargo;
+    }
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
+        Map<String, List<Pessoa>> pessoasPorCargo = pessoas.stream()
+            .collect(Collectors.groupingBy(Pessoa::getCargo,
+                Collectors.filtering(p -> p.getIdade() > 40, 
+                    Collectors.toList())));
+
+            return pessoasPorCargo;
+    }
+}
