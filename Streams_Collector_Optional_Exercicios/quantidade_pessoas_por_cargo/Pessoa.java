b444df8585065f3f011b79a026081759f521c3f1
Eliane Cristina Kobayakawa <az204-elicrisko@outlook.com>
2025-08-21 18:12:55 +0000

    task 1. Contagem de pessoas por cargo

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/Pessoa.java
@@ -0,0 +1,38 @@
+import java.util.Locale;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public String getCargo(){
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    
+    @Override
+    public String toString() {
+        Locale.setDefault(new Locale("pt", "BR"));
+        return String.format(Locale.getDefault(), "[%d] %s %s %d R$ %.6f", 
+            this.codigo, this.nome, this.cargo, this.idade, this.salario);    
+    }
+
+    @Override
+    public int compareTo(Pessoa pessoa) {
+        return this.cargo.compareTo(pessoa.cargo) == 0 ? this.nome.compareTo(pessoa.nome)
+        : this.cargo.compareTo(pessoa.cargo);
+    }
+}
