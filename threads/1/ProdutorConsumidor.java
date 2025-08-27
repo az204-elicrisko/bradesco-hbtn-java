e77684eac7db055e9e7278e9a29c38e296fdf1ef
Eliane Cristina Kobayakawa <eliane.kobayakawa@bradesco.com.br>
2025-08-27 15:01:46 -0300

    task 1. Notificações em Java

---
+++ threads/1/ProdutorConsumidor.java
@@ -0,0 +1,12 @@
+ public class ProdutorConsumidor {
+    public static void main(String[] args) {
+        Buffer buffer = new Buffer();
+        Thread produtor = new Thread(new Produtor(buffer));
+        Thread consumidor = new Thread(new Consumidor(buffer));
+        produtor.start();
+        consumidor.start();
+
+
+    }
+}
+
